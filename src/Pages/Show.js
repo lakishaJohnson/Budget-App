1863cd6458b8351d14880c9bd9dea95a57c536b8
Lakisha J <lakishajohnson@pursuit.org>
2023-07-11 15:36:48 -0400

    Show Route

---
+++ src/Pages/Show.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import TransactionDetails from "../Components/TransactionDetails";
+
+function Show() {
+
+  return (
+    <div>
+      <section>
+        <TransactionDetails />
+      </section>
+    </div>
+  );
+}
+
+export default Show;
