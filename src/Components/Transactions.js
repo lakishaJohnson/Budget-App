aa3acff1e1158c54f0919974fbd7bb8fd95852bc
Lakisha J <lakishajohnson@pursuit.org>
2023-07-12 10:18:17 -0400

    Modified totalAmount logic

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+// import NewForm from "./NewForm";
 import "../budget.css";
 
 function Transactions() {
@@ -8,28 +10,28 @@ function Transactions() {
   const URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
-    axios.get(`${URL}/transactions`)
+    axios
+      .get(`${URL}/transactions`)
       .then((response) => {
         setTransactions(response.data);
         const amountSum = response.data.reduce(
-          (sum, transaction) => sum + transaction.amount,
+          (sum, transaction) => sum + parseFloat(transaction.amount),
           0
         );
-        setTotalAmount(amountSum);
+        setTotalAmount(amountSum.toFixed(2));
       })
       .catch((error) => {
         console.error("Error fetching transactions:", error);
       });
   }, [URL]);
-  
 
   return (
     <div className="transactions-container">
-      <h1 className="account-total">Bank Account Total: {totalAmount}</h1>
+      <h1 className="account-total">Bank Account Total: ${totalAmount}</h1>
       <table className="custom-table">
         <tbody>
-          {transactions.map((transaction) => (
-            <tr className="table-row" key={transaction.id}>
+          {transactions.map((transaction, index) => (
+            <tr className="table-row" key={index}>
               <td className="table-cell">{transaction.date}</td>
               <td className="table-cell">
                 <a href={`/transactions/${transaction.id}`}>
