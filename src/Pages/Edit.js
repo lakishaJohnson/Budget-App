1d7da54b78c38e3f8c50f9b2299800f31d497ea4
Lakisha J <lakishajohnson@pursuit.org>
2023-07-12 15:26:03 -0400

    Edit page

---
+++ src/Pages/Edit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EditForm from '../Components/EditForm'
+
+function Edit() {
+  return (
+    <div>
+    <h1 className="New">Edit item</h1>
+    <EditForm />
+  </div>
+  )
+}
+
+export default Edit
