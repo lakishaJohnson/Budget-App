88c88580651f68130d5d6e2358d8d44621a5af90
Lakisha J <lakishajohnson@pursuit.org>
2023-07-12 10:20:58 -0400

    New Form Page

---
+++ src/Pages/New.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NewForm from "../Components/NewForm";
+
+function New() {
+  return (
+    <div>
+      <h1 className="New">Add a new item</h1>
+      <NewForm />
+    </div>
+  );
+}
+
+export default New;
