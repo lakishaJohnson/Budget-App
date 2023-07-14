2662accf90e9f31513204db8826aa9321ddc8f2d
Lakisha J <lakishajohnson@pursuit.org>
2023-07-14 16:41:03 -0400

    All necessary pages and routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //PAGES AKA REACT ROUTES
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
 
 //COMPONENT
 import NavBar from "./NavBar";
@@ -11,6 +15,14 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
+        <main>
+          <Routes>
+            <Route path="/transactions" element={<Index />} />
+            <Route path="/transactions/:id" element={<Show />} />
+            <Route path="/transactions/:id/edit" element={<Edit />} />
+            <Route path="/transactions/new" element={<New />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
